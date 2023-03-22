6bcda14597c38a87d1091f81691078854ae79dac
asalex04 <arc04@mail.ru>
2023-03-22 11:34:31 +0400

    fix error

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,12 @@
 import express from 'express'
 import 'dotenv/config'
 import router from "./routers";
-// import errorHandler from './middleware/ErrorHandlingMiddleware.js'
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
 app.use(express.json())
 app.use('/api', router)
-//Обработка ошибок
-// app.use(errorHandler)
-
 
 
 const startApp = () => {
